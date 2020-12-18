f6286fe93b75f99e385da5893f8d648f1ffbc176
kpc25459 <kpc25459@gmail.com>
2020-12-18 20:17:14 +0100

    dodany WordListAdapter

--- app/src/main/java/net/dev/roomwordsample/MainActivity.kt
+++ app/src/main/java/net/dev/roomwordsample/MainActivity.kt
@@ -2,10 +2,16 @@ package net.dev.roomwordsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = WordListAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
